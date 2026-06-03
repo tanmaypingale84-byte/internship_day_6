5f5f7c03d29fb9e3e298dd28f19fe0656ce387b6
tanmaypingale84-byte <tanmaypingale84@gmail.com>
2026-06-03 10:04:35 -0700

    helo

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("hello from js!");
+
+let name = "tanmay";
+console.log("Hey " + name + "!");
+
+let num1 = 1000;
+
+num1 = 500;
+console.log(num1);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log(typeof "name");
+console.log(typeof "num1");
+
+
+let number;
+console.log(number);
+
+let student_data = {
+    name: "tanmay",
+    roll_no: 131,
+    class: "tyco",
+    division: "A",
+}
+
+console.log(student_data);
+
+let student = ["abc", "def", "abc"];
+console.log(student);
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != "9");
+console.log(1 <= "10");
